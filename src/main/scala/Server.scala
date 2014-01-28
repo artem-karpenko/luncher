0bfdb6cf08270bb947463632e6b6d9e34119e665
Artem Karpenko <gooyozh@gmail.com>
2014-01-29 00:42:04 +0200

    Printing DDL in response to GET request

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,13 +1,27 @@
+import java.io.{StringWriter, PrintWriter}
+import java.sql.DriverManager
+import org.squeryl.adapters.H2Adapter
+import org.squeryl.{Session, SessionFactory}
 import unfiltered.request.{Seg, Path}
 import unfiltered.response.ResponseString
+import org.squeryl.PrimitiveTypeMode._
 
 /**
  * @author artem
  */
 object Server {
   def main(args: Array[String]) {
+    Class.forName("org.h2.Driver")
+    SessionFactory.concreteFactory =
+      Some(() => Session.create(DriverManager.getConnection("jdbc:h2:d:/tmp/luncher"), new H2Adapter()))
+
     val echo = unfiltered.filter.Planify {
-      case Path(Seg(p :: Nil)) => ResponseString(p)
+      case Path(Seg(p :: Nil)) =>
+        val sw: StringWriter = new StringWriter()
+        transaction {
+          Luncher.printDdl(new PrintWriter(sw))
+        }
+        ResponseString(sw.toString)
     }
 
     unfiltered.jetty.Http.anylocal.filter(echo).run()
